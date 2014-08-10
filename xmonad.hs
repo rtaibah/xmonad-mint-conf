868c1c857d95d1322614485011204ccff092f685
Rami Taibah <taibah@gmail.com>
2014-08-11 00:30:48 +0300

    Add brightness keybindings using xcalib

--- xmonad.hs
+++ xmonad.hs
@@ -216,6 +216,8 @@ myKeyBindings =
     , ((0, 0x1008FF12), spawn "amixer -q set Master toggle")
     , ((0, 0x1008FF11), spawn "amixer -q set Master 10%-")
     , ((0, 0x1008FF13), spawn "amixer -q set Master 10%+")
+    , ((0, 0x1008ff03), spawn "xcalib -co 90 -a")
+    , ((0, 0x1008ff02), spawn "xcalib -c")
   ]
 
 
